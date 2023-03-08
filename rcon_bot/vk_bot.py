efac69bde049e7f39c249351683d4f2c3ee3e924
TynaevDinnerbone <127063453+TynaevDinnerbone@users.noreply.github.com>
2023-03-09 01:28:31 +0600

    Update vk_bot.py

--- rcon_bot/vk_bot.py
+++ rcon_bot/vk_bot.py
@@ -1,28 +1,61 @@
-import re
+import vk_api
+import rcon
+from vk_api.longpoll import VkLongPoll, VkEventType
 
-rcon_session = []
-blocked_commands = []
+vk_session = []
+allowed_users = []
+enable_group = False
+command_symbol = "/"
 
 def connect(session):
-	global rcon_session
-	rcon_session = session
+	global vk_session
+	vk_session = session
 
 def settings(setting, answer):
-	if setting == "blocked_commads":
-		global blocked_commands
-		blocked_commands = [command.lower() for command in answer]
+	if setting == "allowed_users":
+		global allowed_users
+		allowed_users = answer
+	if setting == "group_enable":
+		global enable_group
+		enable_group = answer
+	if setting == "command_symbol":
+		global command_symbol
+		command_symbol = answer
 
-def send_command(command):
-	try:
-		command = command.split()
-		if command[0].lower() in blocked_commands:
-			return "Команда запрещена!"
-		command = " ".join(command)
-		answer = rcon_session.command(command)
-		answer = re.sub(re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]"), "", answer)
-		if not answer:
-			answer = "Ваша команда выполнена!" #На случай если сервер ничего не вернет
-		return answer
-	except Exception as error:
-		print(f"ОШИБКА | [RCON] {error}")
-		exit()
+def execute(command):
+	cmd = rcon.send_command(command)
+	return cmd
+
+def handle_message(event):
+	if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+		if not(event.user_id in allowed_users):
+			return
+		vk = vk_session.get_api()
+		response = vk.messages.getConversationsById(peer_ids = event.peer_id)
+		msg = event.text
+		type = response["items"][0]["peer"]["type"]
+		if type == "user":
+			cmd = execute(msg)
+			vk.messages.send(
+				user_id = event.user_id,
+				message = cmd,
+				random_id = 0
+			)
+			return
+		else:
+			if not(enable_group):
+				return
+			if msg[0] != command_symbol:
+				return
+			cmd = execute(msg[1:])
+			vk.messages.send(
+				peer_id = event.peer_id,
+				message = cmd,
+				random_id = 0
+			)
+
+def run():
+	longpoll = VkLongPoll(vk_session)
+	print("ИНФО | [VK] Успешное подключение")
+	for event in longpoll.listen():
+		handle_message(event)
