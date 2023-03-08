4c2cf4bb56853eb38ac5818c9e397225084410bb
TynaevDinnerbone <127063453+TynaevDinnerbone@users.noreply.github.com>
2023-03-09 01:29:02 +0600

    Update rcon.py

--- rcon_bot/rcon.py
+++ rcon_bot/rcon.py
@@ -10,16 +10,18 @@ def connect(session):
 def settings(setting, answer):
 	if setting == "blocked_commads":
 		global blocked_commands
-		blocked_commands = answer
+		blocked_commands = [command.lower() for command in answer]
 
 def send_command(command):
 	try:
 		command = command.split()
-		if command[0] in blocked_commands:
+		if command[0].lower() in blocked_commands:
 			return "Команда запрещена!"
 		command = " ".join(command)
 		answer = rcon_session.command(command)
 		answer = re.sub(re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]"), "", answer)
+		if not answer:
+			answer = "Ваша команда выполнена!" #На случай если сервер ничего не вернет
 		return answer
 	except Exception as error:
 		print(f"ОШИБКА | [RCON] {error}")
